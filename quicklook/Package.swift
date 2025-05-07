65fb42e7e9948ba01706bbcb8c80055a952a132b
Ben Iofel <ben@iofel.me>
2025-05-07 16:25:52 -0400

    Fix entrypoints and enable thumbnail provider

--- quicklook/Package.swift
+++ quicklook/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.4
 import PackageDescription
 
 let package = Package(
@@ -10,18 +10,36 @@ let package = Package(
         .executable(
             name: "MuseScoreQLPreviewProvider",
             targets: ["MuseScoreQLPreviewProvider"]
+        ),
+        .executable(
+            name: "MuseScoreThumbnailProvider",
+            targets: ["MuseScoreThumbnailProvider"]
         )
     ],
     dependencies: [
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.0"))
     ],
     targets: [
-        .target(
+        .executableTarget(
             name: "MuseScoreQLPreviewProvider",
             dependencies: ["ZIPFoundation"],
             path: "mscz-ql-plugin",
             swiftSettings: [
-                .unsafeFlags(["-Onone"])
+                .unsafeFlags(["-application-extension"])
+            ],
+            linkerSettings: [
+                .unsafeFlags(["-Xlinker", "-e", "-Xlinker", "_NSExtensionMain"])
+            ]
+        ),
+        .executableTarget(
+            name: "MuseScoreThumbnailProvider",
+            dependencies: ["ZIPFoundation"],
+            path: "mscz-thumb-plugin",
+            swiftSettings: [
+                .unsafeFlags(["-application-extension"])
+            ],
+            linkerSettings: [
+                .unsafeFlags(["-Xlinker", "-e", "-Xlinker", "_NSExtensionMain"])
             ]
         )
     ]
--- quicklook/mscz-ql-plugin/main.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-@_silgen_name("NSExtensionMain")
-func _NSExtensionMain(argc: Int32, argv: UnsafeMutablePointer<UnsafeMutablePointer<Int8>?>)
-
-_NSExtensionMain(argc: Int32(CommandLine.argc), argv: CommandLine.unsafeArgv)
